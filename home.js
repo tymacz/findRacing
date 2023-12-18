0a2fc27a959c59c5935b8948e425005cb671d5ec
tymacz <maxence.rebours@outlook.fr>
2023-12-18 21:55:11 +0100

    mise à jour

--- home.js
+++ home.js
@@ -23,7 +23,7 @@ function username() {
                         for (const [key, value] of Object.entries(objet)) {
                             // Crée un élément de liste (li) pour chaque propriété
                             const propertyLi = document.createElement("li");
-                            propertyLi.textContent = `${key}: ${value}`;
+                            propertyLi.textContent = `${value}`;
                     
                             // Ajoute l'élément de liste de propriété à la liste non ordonnée
                             ul.appendChild(propertyLi);
@@ -49,37 +49,42 @@ bt.addEventListener("click",(event)=> {event.preventDefault(); search()});
 
 function search(){
     eraser();
-    const url = window.location.search
-    const urlParams = new URLSearchParams(url)
-    const usr = urlParams.get('user')
+    const url = new URL(window.location.href);
+    const Params = new URLSearchParams(url.search)
+    console.log(url)
+    const usr = Params.get('user')
+    console.log(usr)
     const search = document.getElementById("search").value
-    document.location.replace(`http://127.0.0.1:8081/home.html/?user=${usr}&?search=${search}`);
+    const newUrl = window.location.pathname + `?user=${usr}&search=${search}`;
+    console.log(search)
+    history.pushState({ path: newUrl }, "", newUrl);
     fetch(`http://localhost:8000/joueur/${usr}&${search}`)
     .then(response => response.json())
     .then(data => {
     console.log(data)
     data.forEach(function (objet) {
-        const li = document.createElement("li")
-        const ul = document.createElement("ul");
+            const li = document.createElement("li");
+            const ul = document.createElement("ul");
 
-        // Itère sur les propriétés de l'objet
-        for (const [key, value] of Object.entries(objet)) {
-            // Crée un élément de liste (li) pour chaque propriété
-            const propertyLi = document.createElement("li");
-            propertyLi.textContent = `${key}: ${value}`;
-    
-            // Ajoute l'élément de liste de propriété à la liste non ordonnée
-            ul.appendChild(propertyLi);
-        }
-    
-        // Ajoute la liste non ordonnée à l'élément li
-        li.appendChild(ul);
-    
-        // Ajoute l'élément li à la liste existante dans l'élément avec l'id "joueur"
-        document.getElementById("joueur").appendChild(li);
-        document.getElementById("joueur").appendChild(li);
+            // Itère sur les propriétés de l'objet
+            for (const [key, value] of Object.entries(objet)) {
+                // Crée un élément de liste (li) pour chaque propriété
+                const propertyLi = document.createElement("li");
+                propertyLi.textContent = `${value}`;
+
+                // Ajoute l'élément de liste de propriété à la liste non ordonnée
+                ul.appendChild(propertyLi);
+            }
+
+            // Ajoute la liste non ordonnée à l'élément li
+            li.appendChild(ul);
+
+            // Ajoute l'élément li à la liste existante dans l'élément avec l'id "joueur"
+            document.getElementById("joueur").appendChild(li);
 });
-})}
+})
+.catch((error) => console.error('Erreur lors de la requête API :', error));
+}
 
 
 const simi = document.getElementById("simi");
@@ -91,5 +96,4 @@ function eraser(){
         joueur.removeChild(joueur.firstChild);
       }
 }
-
 window.onload = username()
